f9dc9a986c684b1d378078ddabb41ba74637ac1f
duong <duongduc110103@gmail.com>
2023-10-23 08:50:48 +0700

    a

--- src/app/models/HoaDon.js
+++ src/app/models/HoaDon.js
@@ -2,37 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const HoaDon = new Schema({
-    MaHD: { type: String },
-    User:{
-        UserName:{ type:String},
-        Email:{ type:String},
-        UID:{ type:String},
-        HoTen:{ type:String},
-        SoCCCD:{ type:String},
+    MaHD: { type: String, unique: true },
+    User: {
+        UserName: { type: String },
+        Email: { type: String },
+        UID: { type: String },
+        HoTen: { type: String },
+        SoCCCD: { type: String },
+        SDT: { type: String },
     },
-    Xe:{
-        BKS:{ type:String},
-        HangXe:{ type:String},
-        MauXe:{ type:String},
-        ChuSH:{ type:String},
-        SoGhe:{ type:Number},
-        TinhNang:{ type:[String]},
-        HinhAnh:{ type:[String]},
-        Gia:{type: Number}
+    Xe: {
+        BKS: { type: String },
+        HangXe: { type: String },
+        MauXe: { type: String },
+        ChuSH: { type: String },
+        SoGhe: { type: Number },
+        HinhAnh: { type: Array },
+        Gia: { type: Number }
     },
-    NgayThue: {type: Date},
-    NgayTra: {type: Date},
-    Gia: {type: Number},
-    TienCoc: {type: Number},
-    PhuPhi: {type: Number},
-    ThanhToan: {type: Number},
-    TrangThaiDH: {type: [Number]},
-    TinVat:{
-        Loai:{type:String},
-        HinhAnh:{type:[String]},
-        TrangThai:{type: [Number]}
-    }
+    NgayThue: { type: Date },
+    NgayTra: { type: Date },
+    TongTien: { type: String },
+    PhiDV: { type: String },
+    MaGiamGia: { type: String },
+    GiamGia: { type: Number },
+    TienCoc: { type: Number },
+    PhuPhi: { type: Number },
+    ThanhToan: { type: Number },
+    TrangThaiDH: { type: Number},
+    TinVat: {
+        Loai: { type: String },
+        HinhAnh: { type: [String] },
+        TrangThai: { type: Number }
+    },
+    
 
-},{timestamps:true});
+}, { timestamps: true });
 
-module.exports = mongoose.model("HoaDon",HoaDon);
+module.exports = mongoose.model("HoaDon", HoaDon);
