3b682df2cf0a316916f26197ed94bdcd6beea9f2
DUY MANH <duytqph20012@fpt.edu.vn>
2023-11-05 18:14:22 +0700

    duytq_update_render1

---
+++ src/routes/login.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const loginController = require('../app/controllers/LoginController');
+
+
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: (rep, file, cb) => {
+        cb(null, 'src/public/images');
+    },
+    filename: (rep, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+router.get('/',loginController.index);
+
+ 
+
+
+module.exports = router;
