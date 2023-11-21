1140ce4c10513e51ddef38f60eceb41d85e0f006
DUY MANH <duytqph20012@fpt.edu.vn>
2023-11-22 00:14:30 +0700

    duytq_update_thongbao

--- src/routes/ThongBao.js
+++ src/routes/ThongBao.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const ThongBaoController = require('../app/controllers/ThongBaoCotroller');
- 
+var multer = require('multer');
 
-// router.get('/',ThongBaoController.findNotifi);
-router.use('/',ThongBaoController.index);
- 
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'src/public/images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
 
+
+const upload = multer({ storage: storage });
+
+
+
+router.get('/',ThongBaoController.index);
+router.get('/find/:id',ThongBaoController.find_id);
+router.post('/create',upload.single('HinhAnh'),ThongBaoController.CreateThongBao);
 
 module.exports = router; 
