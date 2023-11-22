102ca1620e21cf26fc3882444648ebe6112ee28c
VanDuc11 <duongduc110103@gmail.com>
2023-11-22 11:00:57 +0700

    ducdv update chuyen

--- src/routes/Hoadon_.js
+++ src/routes/Hoadon_.js
@@ -13,17 +13,12 @@ var storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 router.get('/',HoaDonController_.index);
-router.get('/hoatdong',HoaDonController_.chuyen_hd);
-router.get('/dacoc',HoaDonController_.chuyen_dc);
-router.get('/thanhcong',HoaDonController_.chuyen_TC);
-router.get('/chococ',HoaDonController_.chuyen_CC);
-router.get('/huy',HoaDonController_.chuyen_Huy);
+
 
 router.get('/ChiTietChuyen/:id',HoaDonController_.chitietHD);
 router.get('/list',HoaDonController_.find_hoadon);
 
 
-router.post('/find_theoNgay',HoaDonController_.find_theoNgay);
 router.post('/create',HoaDonController_.create_Hoadon);
 // router.post('/find_user_hoadon',HoaDonController_.find_HoaDon_User);
 router.post('/update_trangthaiHD/:maHD',HoaDonController_.update_trangthaiDH);
